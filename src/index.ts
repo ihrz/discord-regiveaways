4fdd60d97e7a2299656fea8074e3b25cccd0a425
Anaïs Saraiva <53227802+Kisakay@users.noreply.github.com>
2024-01-21 15:57:03 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -17,6 +17,7 @@ import { Data } from './types/Data';
 
 import * as date from 'date-and-time';
 import db from './db.js';
+import { Finnish } from './eventManager';
 
 function GiveawaysManager_Init(client: Client) {
     Refresh(client);
@@ -29,26 +30,27 @@ function GiveawaysManager_Init(client: Client) {
 async function Refresh(client: Client) {
     let drop_all_db = await db.GetAllGiveawaysData();
 
-    for (let data in drop_all_db) {
+    for (let giveawayId in drop_all_db) {
         let now = new Date().getTime();
-        let gwExp = new Date(drop_all_db[data].expireIn).getTime();
+        let gwExp = new Date(drop_all_db[giveawayId].expireIn).getTime();
         let cooldownTime = now - gwExp;
 
         if (now >= gwExp) {
-            // Finnish(
-            //     client,
-            //     messageId,
-            //     guildId,
-            //     channelId
-            // );
+            Finnish(
+                client,
+                giveawayId,
+                drop_all_db[giveawayId].guildId,
+                drop_all_db[giveawayId].channelId
+            );
         };
 
         if (cooldownTime >= 345_600_000) {
-            // db.delete(`GIVEAWAYS.${guildId}.${channelId}.${messageId}`);
+            db.DeleteGiveaway(giveawayId)
         };
     }
 };
 
 export {
     GiveawaysManager_Init,
+    Refresh
 };
